f58e6dea490805a71a1be81f38c8d05633e153ef
Roel Barreto <roel.mail73@gmail.com>
2016-12-27 08:49:59 +0800

    Fixed: Test fails on local font files

--- internals/webpack/webpack.test.babel.js
+++ internals/webpack/webpack.test.babel.js
@@ -31,7 +31,7 @@ module.exports = {
         loader: 'babel',
         exclude: [/node_modules/],
       },
-      { test: /\.jpe?g$|\.gif$|\.png$|\.svg$/i,
+      { test: /\.jpe?g$|\.gif$|\.png$|\.svg|\.woff|\.woff2|\.eot|\.ttf$/i,
         loader: 'null-loader',
       },
     ],
